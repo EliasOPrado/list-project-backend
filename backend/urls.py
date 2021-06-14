f879eb31dcc77781e4383f3b8f477ee4386d80b1
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-06-14 01:07:07 -0300

    updated url

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .viewsets import UserViewSet, GroupViewSet
 from core.api.viewsets import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -28,5 +29,5 @@ router.register(r'item', ItemViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
 ]
